580c322d2ea34468bc4e32e1b83579ecc9e18b9e
chienphan525 <chienphan525@gmail.com>
2026-09-09 17:22:59 +0700

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -39,8 +39,10 @@ const MobileNav = () => {
 
       {/* Menu mở */}
       <div
-        className={`fixed left-0 top-0 z-50 w-full transform shadow-2xl duration-300 ease-in-out ${
-          navShow ? 'translate-x-0' : 'translate-x-full'
+        className={`fixed left-0 top-0 z-50 w-full shadow-2xl transition-all duration-500 ease-out ${
+          navShow
+            ? 'scale-100 opacity-100'
+            : 'pointer-events-none scale-0 opacity-0'
         }`}
         style={{
           height: '50vh',
