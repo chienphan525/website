578e3be6ead44564c6846eb4234c238772862e10
chienphan525 <chienphan525@gmail.com>
2026-09-09 12:10:34 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import MobileNav from './MobileNav'
 export default function Header() {
   return (
     <header className="cp-header">
-      <div className="flex items-center justify-between px-5 py-4 sm:px-8">
+      <div className="flex items-center justify-between px-5 py-4 sm:px-3">
         <Link href="/" aria-label="Chiến Phan">
           <img src="/static/chienphan/logo.png" alt="Chiến Phan" className="h-10 w-auto sm:h-12" />
         </Link>
