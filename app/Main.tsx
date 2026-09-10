aa2d9f5ea3a89ecce064b42ddfee7bfbde45b17a
chienphan525 <chienphan525@gmail.com>
2026-09-10 11:23:34 +0700

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
           <p className="cp-kicker">CHIẾN PHAN</p>
           <h1>
             <span className="block lg:inline">Chạm nhẹ từng</span>
-            <span className="ml-2 block lg:inline">khoảnh khắc,</span>
+            <span className="block lg:ml-2 lg:inline">khoảnh khắc,</span>
             <br className="hidden lg:block" />
             <span className="block">mọi áp lực sẽ Tan</span>
           </h1>
