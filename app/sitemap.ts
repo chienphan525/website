bf78071f701f58c877f3939a7017a7436312936c
chienphan525 <chienphan525@gmail.com>
2026-09-13 09:30:44 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,7 +12,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
+  const routes = [
+    '',
+    'blog',
+    'nhat-ky',
+    'doi-song',
+    'gia-tri-song',
+    'review',
+    'san-pham',
+    'ket-noi',
+    'projects',
+    'tags',
+    ].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
