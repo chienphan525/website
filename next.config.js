27f2af31f06a59f45470916e1be8b681280de0a3
chienphan525 <chienphan525@gmail.com>
2026-09-15 14:56:28 +0700

    Fix Tawk.to CSP for mobile

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google-analytics.com https://www.googletagmanager.com https://vercel.live giscus.app;
-  style-src 'self' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google-analytics.com https://www.googletagmanager.com https://vercel.live https://*.tawk.to giscus.app;
+  style-src 'self' 'unsafe-inline' https://*.tawk.to giscus.app;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
-  frame-src https://vercel.live giscus.app;
-  frame-ancestors 'self' giscus.app
+  font-src 'self' https://*.tawk.to;
+  frame-src https://vercel.live https://*.tawk.to giscus.app;
+  frame-ancestors 'self' giscus.app;
 `
 
 const securityHeaders = [
